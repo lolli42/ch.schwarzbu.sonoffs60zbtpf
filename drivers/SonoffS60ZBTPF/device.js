ddc4425a3ad06698b1e13ffa4e92646941fe69dd
Christian Kuhn <lolli@schwarzbu.ch>
2025-12-06 18:21:58 +0100

    [TASK] Register attribute reporting for on/off state
    When the device on/off switch is toggled, the device
    is now configured to send data to homey. This is much
    more elegant than polling the device every some seconds
    for its state.

--- drivers/SonoffS60ZBTPF/device.js
+++ drivers/SonoffS60ZBTPF/device.js
@@ -7,12 +7,32 @@ debug(true);
 
 class SonoffS60ZBTPF extends ZigBeeDevice {
   async onNodeInit({ zclNode }) {
+    this.log('init');
     if (this.hasCapability('onoff')) {
       this.registerCapability(
         'onoff',
         CLUSTER.ON_OFF,
-        { getOpts: { pollInterval: 15000, getOnOnline: true } }
+        {
+          // Handle the device on/off switch to report state back to homey when toggled:
+          // a) lame solution with getOpts: actively poll all x milliseconds
+          //getOpts: { pollInterval: 15000, getOnOnline: true },
+          // b) register reporting attribute so device actively sends changed on/off state to homey
+          //report: 'onOff', // unsure what this is good for.
+          //reportParser(report) { // debugging reported value
+          //  this.log(report);
+          //  return report === true;
+          //},
+          reportOpts: {
+            configureAttributeReporting: {
+              minInterval: 1, // Device does not send data when on/off switch is
+                              // toggled for often than once a second. This is the
+                              // min value from interview.
+              maxInterval: 600, // Not 100% sure how this works.
+            },
+          },
+        }
       );
+
     }
   }
 }
